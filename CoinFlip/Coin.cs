85bd741e0185563b5c36f8238e4b6baf5b2a58d9
Melltiy <lauramouraferrarii2005@gmail.com>
2025-04-11 22:30:39 -0300

    Continuação_CoinFlip

---
+++ CoinFlip/Coin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinFlip
+{
+    public class Coin
+    {
+        private string LadoSorteado;
+
+        public string LadoSorteado1 { get => LadoSorteado; set => LadoSorteado = value; }
+        public Coin()
+        {
+
+        }
+
+
+
+        public String Flip()
+        {
+            Random random = new Random();
+            if (random.Next(2) == 0)
+            {
+                LadoSorteado = "Cara";
+            }
+            else
+            {
+                LadoSorteado = "Coroa";
+            }
+
+           // LadoSorteado = random.Next(2) == 0 ? "Cara" : "Coroa";
+
+
+            return(LadoSorteado);
+        }
+    }
+}
+
+
+        
+
